330d813b9988412d072a3e57f0040aec15e5e792
王琳飞 <linfei6@staff.sina.com.cn>
2018-08-19 20:20:46 +0800

    添加both同步仓库到coding

--- ssfe.js
+++ ssfe.js
@@ -11,6 +11,7 @@ const getDeployConfig = (data) => {
                 enable: true,
                 // 上传的容器盒子，不填为随机路径
                 folder: '',
+                
                 dir: '',
                 pattern: '**/*',
                 // 是否强制替换已有文件
