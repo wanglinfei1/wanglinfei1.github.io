8dd18e2c4d137072871f7ffc549c7f581f5021aa
王琳飞 <linfei6@staff.sina.com.cn>
2018-08-19 23:15:06 +0800

    添加both同步仓库到coding__DEPLOY__

--- ssfe.js
+++ ssfe.js
@@ -23,6 +23,31 @@ const getDeployConfig = (data) => {
     }
     return config;
 };
+const getDeployConfig = (data) => {
+    let config = {
+        // 是否自动部署
+        enable: false
+    };
+    if (data && data.commits && data.commits.length >=0) {
+        let msg = data.commits[0].message;
+        if (msg.indexOf('__DEPLOY__') >= 0) {
+            config = {
+                mailTo: 'wlinfei@sina.com',
+                enable: true,
+                // 上传的容器盒子，不填为随机路径
+                folder: '',
+                dir: '',
+                pattern: '!(_webhook_)',
+                // 是否强制替换已有文件
+                replace: true,
+                // 是否启用缓存
+                cache: true,
+                maxCount:1000,
+            }
+        }
+    }
+    return config;
+};
 let getFTPDeployConfig = (data) => {
     let config = {
         enable: false
