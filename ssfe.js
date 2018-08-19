0a231b9b9b8c6a0630b8812fc0a8faa181f19b0b
王琳飞 <linfei6@staff.sina.com.cn>
2018-08-19 08:43:03 +0800

    __DEPLOY__

--- ssfe.js
+++ ssfe.js
@@ -17,8 +17,7 @@ const getDeployConfig = (data) => {
                 replace: false,
                 // 是否使用缓存，默认为 true 缓存一年；false 缓存一分钟
                 cache: true,
-                maxCount:1000
-        
+                maxCount:1000  
             }
         }
     }
